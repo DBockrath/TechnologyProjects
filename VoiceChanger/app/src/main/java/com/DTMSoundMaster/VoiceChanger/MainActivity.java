64a57c6111092d7726ceb9f1832453c1fea93584
DTMSoundMaster <dtmsoundcorps@gmail.com>
2017-02-11 15:58:01 -0500

    Added VoiceChanger

---
+++ VoiceChanger/app/src/main/java/com/DTMSoundMaster/VoiceChanger/MainActivity.java
@@ -0,0 +1,14 @@
+package com.DTMSoundMaster.VoiceChanger;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

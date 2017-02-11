ac8c5af874780b9eec9848f398cda79ca9ccef64
DTMSoundMaster <dtmsoundcorps@gmail.com>
2017-02-11 11:01:36 -0500

    Updated BluetoothComs to scan for devices. Contains errors

--- BluetoothComs/Application/build/gen/com/DTMSoundMaster/BluetoothComs/R.java
+++ BluetoothComs/Application/build/gen/com/DTMSoundMaster/BluetoothComs/R.java
@@ -14,9 +14,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f070001;
-        public static final int button2=0x7f070002;
-        public static final int button3=0x7f070003;
+        public static final int b1=0x7f070001;
+        public static final int b2=0x7f070002;
+        public static final int b3=0x7f070003;
+        public static final int b4=0x7f070004;
         public static final int out=0x7f070000;
     }
     public static final class layout {
--- BluetoothComs/Application/src/main/java/com/DTMSoundMaster/BluetoothComs/PhoneActivity.java
+++ BluetoothComs/Application/src/main/java/com/DTMSoundMaster/BluetoothComs/PhoneActivity.java
@@ -2,17 +2,27 @@ package com.DTMSoundMaster.BluetoothComs;
 
 import android.app.*;
 import android.os.*;
-import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothSocket;
+import android.bluetooth.BluetoothServerSocket;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.BroadcastReceiver;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.lang.reflect.*;
 
 public class PhoneActivity extends Activity {
 	
@@ -26,9 +36,10 @@ public class PhoneActivity extends Activity {
         setContentView(R.layout.activity_phone);
 		
 		final TextView out = (TextView)findViewById(R.id.out);
-		final Button button1 = (Button)findViewById(R.id.button1);
-		final Button button2 = (Button)findViewById(R.id.button2);
-		final Button button3 = (Button)findViewById(R.id.button3);
+		final Button b1 = (Button)findViewById(R.id.b1);
+		final Button b2 = (Button)findViewById(R.id.b2);
+		final Button b3 = (Button)findViewById(R.id.b3);
+		final Button b4 = (Button)findViewById(R.id.b4);
 		final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 		
 		if (mBluetoothAdapter == null) {
@@ -37,7 +48,7 @@ public class PhoneActivity extends Activity {
 			
 		}
 		
-		button1.setOnClickListener(new View.OnClickListener() {
+		b1.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {
 				
@@ -52,7 +63,7 @@ public class PhoneActivity extends Activity {
 			
 		});
 		
-		button2.setOnClickListener(new View.OnClickListener() {
+		b2.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
@@ -72,16 +83,75 @@ public class PhoneActivity extends Activity {
 			
 		});
 		
-		button3.setOnClickListener(new View.OnClickListener() {
+		b3.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
-			public void onClick(View ar0) {
+			public void onClick(View arg0) {
 				
 				mBluetoothAdapter.disable();
 				
 				// out.append("Turning Off Bluetooth");
 				
 				Toast.makeText(getApplicationContext(), "Turning Off Bluetooth", Toast.LENGTH_LONG);
+				
+			}
+			
+		});
+		
+		b4.setOnClickListener(new View.OnClickListener() {
+			
+				private void showToast(String message) {
+
+					Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+				}
+			
+			@Override
+			public void onClick(View arg0) {
+				
+				BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+				IntentFilter filter = new IntentFilter();
+				
+				filter.addAction(BluetoothDevice.ACTION_FOUND);
+				filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+				filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+				
+				registerReceiver(mReceiver, filter);
+				adapter.startDiscovery();
+				
+			}
+			
+			private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+				
+				public void onReceive(Context context, Intent intent) {
+					
+					String action = intent.getAction();
+					
+					if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+						
+						// Discovery starts, we can now show progress dialogue or perform other tsaks
+						
+					} else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+						
+						// Discovery finishes, dismiss progress dialoogue
+						
+					} else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+						
+						// Bluetooth device found
+						
+						BluetoothDevice device = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+						showToast("Found Device " + device.getName());
+						
+					}
+					
+				}
+				
+			};
+			
+			public void onDestroy() {
+				
+				unregisterReceiver(mReceiver);
+				super.onDestroy();
 				
 			}
 			

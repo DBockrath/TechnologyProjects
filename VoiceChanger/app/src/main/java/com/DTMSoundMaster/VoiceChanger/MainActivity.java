28919bb0b32621f06de219e40a63b2fa3be649e1
DTMSoundMaster <dtmsoundcorps@gmail.com>
2017-02-25 18:13:48 -0500

    Formatted VoiceChanger

--- VoiceChanger/app/src/main/java/com/DTMSoundMaster/VoiceChanger/MainActivity.java
+++ VoiceChanger/app/src/main/java/com/DTMSoundMaster/VoiceChanger/MainActivity.java
@@ -3,12 +3,14 @@ package com.DTMSoundMaster.VoiceChanger;
 import android.app.*;
 import android.os.*;
 
-public class MainActivity extends Activity 
-{
+public class MainActivity extends Activity {
+	
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
+		
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
     }
+	
 }

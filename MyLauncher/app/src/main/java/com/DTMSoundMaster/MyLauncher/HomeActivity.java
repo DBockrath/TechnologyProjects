985078a528351f80b5a7fdc51ce16268bdfa017f
DTMSoundMaster <dtmsoundcorps@gmail.com>
2017-03-14 21:00:44 -0400

    Edited MyLauncher

--- MyLauncher/app/src/main/java/com/DTMSoundMaster/MyLauncher/HomeActivity.java
+++ MyLauncher/app/src/main/java/com/DTMSoundMaster/MyLauncher/HomeActivity.java
@@ -4,8 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class HomeActivity extends Activity {
+	
+	Button mButton = (Button)findViewById(R.id.enter_input);
+	Button aButton = (Button)findViewById(R.id.apps_button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +20,27 @@ public class HomeActivity extends Activity {
         setContentView(R.layout.activity_home);
 
     }
+	
+	aButton.setOnClickListener(new View.OnClickListener() {
+		
+		public void onClick(View v) {
 
-	public void showApps(View v) {
+			Intent i = new Intent(this, AppsListActivity.class);
+			startActivity(i);
 
-		Intent i = new Intent(this, AppsListActivity.class);
-		startActivity(i);
-
-	}
+		}
+		
+	});
+	
+	mButton.setOnClickListener(new View.OnClickListener() {
+	
+		public void enter() {
+		
+			EditText mEdit = (EditText)findViewById(R.id.user_input);
+			mEdit.getText().toString();
+		
+		}
+		
+	});
 
 }

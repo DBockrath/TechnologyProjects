a100f39c7507e9ec29a9c7d347aa4e10e20e3bff
DTMSoundMaster <dtmsoundcorps@gmail.com>
2017-03-15 21:29:54 -0400

    Added SMSApp

---
+++ MySMSApp/app/src/main/java/com/DTMSoundMaster/MySMSApp/SmsReceiver.java
@@ -0,0 +1,17 @@
+package com.DTMSoundMaster.MySMSApp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SmsReceiver extends BroadcastReceiver {
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+		Bundle bundle = intent.getExtras();
+		
+	}
+	
+}

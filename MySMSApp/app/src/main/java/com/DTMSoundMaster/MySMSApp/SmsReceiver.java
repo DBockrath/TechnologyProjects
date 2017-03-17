e0fde63ddb949737753071f89a0b92de2846baf7
DTMSoundMaster <dtmsoundcorps@gmail.com>
2017-03-16 20:18:42 -0400

    Finished Simple Version

--- MySMSApp/app/src/main/java/com/DTMSoundMaster/MySMSApp/SmsReceiver.java
+++ MySMSApp/app/src/main/java/com/DTMSoundMaster/MySMSApp/SmsReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.gsm.SmsMessage;
+import android.widget.*;
 
 public class SmsReceiver extends BroadcastReceiver {
 	
@@ -11,6 +13,28 @@ public class SmsReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		
 		Bundle bundle = intent.getExtras();
+		
+		SmsMessage[] msgs = null;
+		String str = "";
+		
+		if (bundle != null) {
+			
+			Object[] pdus = (Object[])bundle.get("pdus");
+			msgs = new SmsMessage[pdus.length];
+			
+			for (int i = 0; i < msgs.length; i++) {
+				
+				msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+				str += "SMS from: " + msgs[i].getOriginatingAddress();
+				str += " :";
+				str += msgs[i].getDisplayMessageBody().toString();
+				str += "n";
+				
+			}
+			
+			Toast.makeText(context, str,  Toast.LENGTH_SHORT).show();
+			
+		}
 		
 	}
 	

8d96403dd954eedffedf66e1540d8e08a317ae7e
DTMSoundMaster <dtmsoundcorps@gmail.com>
2017-03-16 20:28:53 -0400

    Added XML files

--- MySMSApp/app/build/gen/com/DTMSoundMaster/MySMSApp/R.java
+++ MySMSApp/app/build/gen/com/DTMSoundMaster/MySMSApp/R.java
@@ -14,13 +14,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int btnSendSMS=0x7f060002;
-        public static final int message_field=0x7f060003;
-        public static final int txtMessage=0x7f060001;
-        public static final int txtPhoneNo=0x7f060000;
+        public static final int btnSendSMS=0x7f060004;
+        public static final int home_list_view=0x7f060000;
+        public static final int message_field=0x7f060005;
+        public static final int new_message_button=0x7f060001;
+        public static final int txtMessage=0x7f060003;
+        public static final int txtPhoneNo=0x7f060002;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int activity_home=0x7f030000;
+        public static final int list_item=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
